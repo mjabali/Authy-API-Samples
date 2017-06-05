ce11bd9479b96a4a787328907f23adc0d2fe7f8a
Marcelo Jabali <mqjabali@gmail.com>
2017-06-05 10:47:23 -0700

    Fixed deleteUser.sh|bat request format call

--- deleteUser.bat
+++ deleteUser.bat
@@ -3,7 +3,7 @@ cls
 echo Delete the User
 echo.
 echo Request
-echo curl -X POST https://api.authy.com/protected/%%AUTHY_API_FORMAT%%/users/%%AUTHY_ID%%/delete \
+echo curl -X POST https://api.authy.com/protected/%%AUTHY_API_FORMAT%%/users/%%AUTHY_ID%%/delete
 echo -H "X-Authy-API-Key: %%AUTHY_API_KEY%%"
 echo.
 echo Response
--- deleteUser.sh
+++ deleteUser.sh
@@ -2,7 +2,7 @@ clear
 echo Delete the User
 echo
 echo Request
-echo curl -X POST https://api.authy.com/protected/\$AUTHY_API_FORMAT/users/\$AUTHY_ID/delete \
+echo curl -X POST https://api.authy.com/protected/\$AUTHY_API_FORMAT/users/\$AUTHY_ID/delete
 echo -H "X-Authy-API-Key: \$AUTHY_API_KEY"
 echo
 echo Response
