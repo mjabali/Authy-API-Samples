e9b8b39bf106cb3d58ffa5c2ee3367b6049683ce
Marcelo Jabali <mqjabali@gmail.com>
2017-06-08 12:15:28 -0700

    Fixed typo

--- phoneVerificationRequest.sh
+++ phoneVerificationRequest.sh
@@ -2,8 +2,8 @@ clear
 echo Sending Phone Verification Request
 echo
 echo Request
-echo curl "https://api.authy.com/protected/$AUTHY_API_FORMAT/phones/verification/start?api_key=\$AUTHY_API_KEY
-e"cho -d via=SMS or CALL
+echo curl "https://api.authy.com/protected/$AUTHY_API_FORMAT/phones/verification/start?api_key=\$AUTHY_API_KEY"
+echo -d via=SMS or CALL
 echo -d country_code=\$USER_COUNTRY
 echo -d phone_number=\$PHONE_NUMBER
 echo
